5bf1de8af7ff353c63c93b5b6497f6d4e17dd0e1
Gaurav Gupta <gdgupta11@hotmail.com>
2021-01-11 15:48:39 +0530

    Update site.pp
    Adding another node for DB server and again uncommenting default values.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,17 @@
-node default {
+ node default {
   file {'/root/README':
    ensure   => file,
    content  => 'This is a readme file',
    owner    => 'root',
    }
+}
+
+node default{
+}
+node 'web'{
+  include role::master_server
+}
+
+node 'db' {
+  include role::db_server
 }
